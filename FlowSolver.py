9f1e0ccea01c16f2b46c135b8ae942c57304bd73
ripole <133680683+ripole@users.noreply.github.com>
2024-07-29 18:59:50 -0400

    added and board_solver

--- FlowSolver.py
+++ FlowSolver.py
@@ -1,4 +1,5 @@
 import flowController
+import numpy as np
 
 gameBoard = flowController.Controller('8by8Puzzles.txt')
 
@@ -23,3 +24,59 @@ def evaluateBoard():
             pointB = findEndNode(roots[1])
             distance += abs(pointA.x - pointB.x) + abs(pointA.y - pointB.y)
     return distance + len(completedColors)
+
+
+
+def board_solver_simulated_annealing(controller):
+    board = controller.board_obj
+
+    temperature = 100
+
+    cooling_rate = .999
+
+    current_score = heuristic(board)
+    
+    roots = board.rootMap
+
+    while temperature > 1:
+
+        moves = {
+            0: (-1, 0),  # up
+            1: (1, 0),   # down
+            2: (0, -1),  # left
+            3: (0, 1)    # right
+        }
+
+        # 0 will be up, 1 down, 2 left, 3 right
+        next_move = np.random.choice(0,1,2,3)
+
+        new_i,new_j = moves[next_move]
+
+        selected_color = np.random.choice(list(roots.keys()))
+
+        selected_root = roots[selected_color][-2]
+
+        new_node_i = selected_root.x+new_i
+        new_node_j = selected_root.x+new_j
+
+        try:
+            board.extendPath(selected_root.x,selected_root.y,new_node_i,new_node_j)
+            updated_score = heuristic(board)
+
+            if updated_score < current_score or np.random.uniform(0, 1) <= np.exp((current_score - updated_score) / temperature):
+                current_score = updated_score
+                roots[selected_color].insert(len(roots[selected_color]) - 1, (new_node_i,new_node_j))
+            else:
+                board.removeNode(new_i,new_j)
+            temperature *= cooling_rate
+        except:
+            continue
+
+
+
+
+
+
+        
+
+
